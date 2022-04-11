a7947b69f56fc7d64b53fc87deeeae9daee16209
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2022-04-11 11:27:39 -0300

    tablet interface

--- lib/pages/widgets/sections/stop_section.dart
+++ lib/pages/widgets/sections/stop_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_app/breakpoints.dart';
 import 'package:responsive_app/pages/widgets/custom_search_field.dart';
 
 class TopSection extends StatelessWidget {
@@ -48,6 +49,49 @@ class TopSection extends StatelessWidget {
                     ),
                   ),
                 )
+              ],
+            ),
+          );
+        }
+
+        if (maxWidth >= mobileBreakpoint) {
+          return SizedBox(
+            height: 320,
+            width: double.infinity,
+            child: Stack(
+              children: [
+                SizedBox(
+                  child: Image.network(
+                    'https://i0.wp.com/nscreen.com.br/wp-content/uploads/2021/08/Padrao-Hort-BrancoNR.png?fit=4018%2C971&ssl=1',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 20,
+                  child: Card(
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      width: 350,
+                      child: Column(
+                        children: const <Widget>[
+                          Text(
+                            'Aprenda flutter trtr rjrhjr rjhjrh',
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                              'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'),
+                          SizedBox(height: 8),
+                          CustomSearhField(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           );
